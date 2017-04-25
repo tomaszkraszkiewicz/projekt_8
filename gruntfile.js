2191016e84daf4bb9e960a60e16fc7b309d5693d
tomaszkraszkiewicz <tomasz.kraszkiewicz@gmail.com>
2017-04-25 11:17:42 +0200

    upload zadania 8_3 kodila

--- gruntfile.js
+++ gruntfile.js
@@ -3,8 +3,19 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		jshint: {
 			all: ['js/*.js']
+		},
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'css/main.css': 'sass/main.sass'
+				}
+			}
 		}
 	});
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.registerTask('default', ['jshint']);
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.registerTask('default', ['jshint', 'sass']);
 }
